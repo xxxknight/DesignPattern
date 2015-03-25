e9c5d1b30eb3abef73a1269072fe43ee9499dd08
xxxknight <xingshiyang24@163.com>
2015-03-25 23:21:11 +0800

    ass

---
+++ src/Singleton/SingletonTest.java
@@ -0,0 +1,25 @@
+package Singleton;
+
+public class SingletonTest {
+
+	public static void main(String[] args) {
+		Singleton singleton1 = Singleton.getInstance();
+		Singleton singleton2 = Singleton.getInstance();
+		System.out.println(singleton1 == singleton2);
+	}
+}
+
+class Singleton {
+	private Singleton() {
+
+	}
+
+	private static Singleton singleton;
+
+	public static Singleton getInstance() {
+		if (singleton == null) {
+			singleton = new Singleton();
+		}
+		return singleton;
+	}
+}
---
+++ src/Singleton/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author sy24
+ *
+ */
+package Singleton;
